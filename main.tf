5d1604d27ebec3d6c8233616459551d0db522198
Hardcorelevelingwarrior <92463182+Hardcorelevelingwarrior@users.noreply.github.com>
2022-10-16 11:33:51 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,42 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
+  backend "s3" {
+    bucket         = "terraform-series-meomeo-s3-backend"
+    key            = "terraform-jenkins"
+    region         = "us-west-2"
+    encrypt        = true
+    role_arn       = "arn:aws:iam::682220946551:role/Terraform-Series-MeomeoS3BackendRole"
+    dynamodb_table = "terraform-series-meomeo-s3-backend"
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Server"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.server.public_ip
 }
